6c0ae3a558c6c08414243960410556bf5603539d
flower0805 <kenny3191837252@gmail.com>
2022-12-24 04:41:33 +0800

    exceptAR other ok

---
+++ lipstick_app/Assets/code/Change_scene.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Change_scene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Change1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Change2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Change3()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Change4()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void Change5()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void Change6()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+}
